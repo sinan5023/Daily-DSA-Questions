58c570f556b64f50c7a964bd5f8dce9f99c5e71e
Mohammed Sinan <sinan@mohammeds-MacBook-Air.local>
2025-09-08 11:13:34 +0530

    day 19

---
+++ prefix.js
@@ -0,0 +1,15 @@
+// Write a function that, given a list of words and a prefix, returns all words that start with that prefix.
+
+// Example:
+// Input: words = ["facebook", "face", "factory", "fast", "dog"], prefix = "fa"
+// Output: ["facebook", "face", "factory", "fast"]
+let arr = ["facebook", "face", "factory", "fast", "dog"]
+let prefix = 'face'
+
+let firstTwo = arr.map(str => str.slice(0,prefix.length));
+console.log(firstTwo)
+for (let i = 0; i < arr.length; i++) {
+    if(firstTwo[i]==prefix){
+        console.log(arr[i])
+    }
+}
